bd2b3375a2d9ddfbfe51d23ee977e58877ab9301
DevaNandaRadhakrishnan <287636@ust.com>
2024-10-21 12:15:35 +0530

    committed

---
+++ src/main/java/com/ust/bookservice/entity/Book.java
@@ -0,0 +1,73 @@
+package com.ust.bookservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String authorId;
+    private LocalDate publishedDate;
+    private String imageUrl;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String authorId, LocalDate publishedDate, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+        this.publishedDate = publishedDate;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
